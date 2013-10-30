ade0a3976ba4fd280ef47246e03d8ea60b228519
angeljara <angeljaraplaza@gmail.com>
2013-10-30 16:13:53 +0100

    he modificado el programa DecimalBinario.py

--- test.py
+++ test.py
@@ -3,5 +3,10 @@ import DecimalBinario
 numero = int(raw_input('Introduce el numero decimal a convertir a binario: '))
 print (DecimalBinario.Decimal2Binario(numero))
 
+
+
+
+#Esta parte hace el cambio de binario a decimal:
+
 str = raw_input('Introduce el numero binario a convertir a decimal: ')
 print(DecimalBinario.Binario2Decimal(str))
